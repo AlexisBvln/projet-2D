bf8044490559a2f898a4e2d139c203fd391a8ba8
Alexis B <bovilanos3@hotmail.com>
2017-06-25 22:56:19 +0200

    Premier ajout de la prédiction de la trajectoire de la balle
    Premier ajout de la prédiction de la trajectoire de la balle.
    Ce n'est pas terminé.

--- Assets/MyAssets/Scripts/Canon.cs
+++ Assets/MyAssets/Scripts/Canon.cs
@@ -6,26 +6,28 @@ public class Canon : MonoBehaviour {
 	public GameObject departBall;
 	public GameObject ballPrefab;
 
-	private GameObject newBall;
 	private bool mouseIsOnCanon;
+    private bool temoin;
 
 	// Use this for initialization
 	void Start () {
 		mouseIsOnCanon = false;
-	}
+        temoin = false;
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		// Le canon suis la souris
 		Vector3 pos = Camera.main.WorldToScreenPoint (transform.position);
 		Vector3 dir = Input.mousePosition - pos;
 		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-		if (angle > Global.canonAngleMin && angle < Global.canonAngleMax){
+        if (angle > Global.canonAngleMin && angle < Global.canonAngleMax){
 			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-			// Shoot du ballon
-			if (Input.GetMouseButtonUp (0) && Global.waitForShoot && !mouseIsOnCanon) {
+            // Shoot du ballon
+            if (Input.GetMouseButtonUp (0) && Global.waitForShoot && !mouseIsOnCanon) {
 
 				// On désactive le collider du canon afin de ne pas poser de 
 				// problème avec la balle quand elle sort du canon
@@ -56,26 +58,6 @@ public class Canon : MonoBehaviour {
 				}
 				Global.waitForShoot = false;
 			}
-		}
-	}
-
-	// Afin de prévoir la trajectoire du ballon
-	void UpdateTrajectory(Vector3 initialPosition, Vector3 initialVelocity, Vector3 gravity)
-	{
-		int numSteps = 20; // for example
-		float timeDelta = 1.0f / initialVelocity.magnitude; // for example
-		
-		LineRenderer lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.SetVertexCount(numSteps);
-		
-		Vector3 position = initialPosition;
-		Vector3 velocity = initialVelocity;
-		for (int i = 0; i < numSteps; ++i)
-		{
-			lineRenderer.SetPosition(i, position);
-			
-			position += velocity * timeDelta + 0.5f * gravity * timeDelta * timeDelta;
-			velocity += gravity * timeDelta;
 		}
 	}
 
---
+++ Assets/MyAssets/Scripts/Prediction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prediction : MonoBehaviour {
+
+    protected Vector2 velocity;
+
+    private float gravityModifier = 1.2f;
+    private GameObject[] listDotClone;
+    private int nbDePoint = 500;
+
+    // Use this for initialization
+    void Start()
+    {
+        listDotClone = new GameObject[nbDePoint];
+
+        // Création de tous les points au même endroit avec des noms différents
+        for (int i = 0; i < nbDePoint; i++)
+        {
+            GameObject dotClone = Resources.Load("dotPrediction") as GameObject;
+            dotClone.name = "dotClone_" + i;
+            dotClone = Instantiate(dotClone, this.transform.position, Quaternion.identity) as GameObject;
+            listDotClone[i] = dotClone;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 0.0f;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 shootDirection = mousePosition - this.transform.position;
+        float testMagnitude = new Vector2(shootDirection.x * Constantes.SPEED_BALL, shootDirection.y * Constantes.SPEED_BALL).magnitude;
+        if (testMagnitude < Constantes.MIN_PUISSANCE_CANON)
+        {
+            velocity = new Vector2(shootDirection.x * Constantes.SPEED_BALL * Constantes.MULT_SPEED_BALL, shootDirection.y * Constantes.SPEED_BALL * Constantes.MULT_SPEED_BALL);
+        }
+        else
+        {
+            velocity = new Vector2(shootDirection.x * Constantes.SPEED_BALL, shootDirection.y * Constantes.SPEED_BALL);
+        }
+
+        Vector2 positionStep = transform.position;
+
+        for (int i = 0; i < listDotClone.Length; i++)
+        {
+            velocity += gravityModifier * Physics2D.gravity;
+            positionStep += velocity * Time.deltaTime;
+
+            GameObject dotPredictionTemp = GameObject.Find(listDotClone[i].name);
+            dotPredictionTemp.GetComponent<Transform>().position = positionStep;
+        }
+    }
+}
