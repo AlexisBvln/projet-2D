50022c68c22820c1a51296865bb75d2777134013
Alexis B <bovilanos3@hotmail.com>
2017-06-26 22:39:32 +0200

    merge

--- Assets/MyAssets/Scripts/Basket.cs
+++ Assets/MyAssets/Scripts/Basket.cs
@@ -7,18 +7,6 @@ public class Basket : MonoBehaviour {
     // Quand le ballon attérit dans le panier
     // On sauvegarde le score et on affiche le panel
     void OnTriggerEnter2D(Collider2D col){
-        if (col.gameObject.tag.Equals(Constantes.BALL_TAG) && !Global.isWin){
-            if (Global.point == 3) {
-                Global.isWin = true;
-                GestionLevel.SaveLevel(Global.levelActive, Global.point);
-            }
-            else if (Global.point > 0) {
-                Global.isWin = false;
-                GestionLevel.SaveLevel(Global.levelActive, Global.point);
-            }
-            else {
-                Global.isWin = false;
-                GestionLevel.affichePanelScore();
 
         if (!Global.IsWrongWay)
         {
